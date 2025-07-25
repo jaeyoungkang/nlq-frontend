d3bcfdee557d5418f799f9fc507df1ebbf1e9168
jaeyoungkang <jaeyoung2010@gmail.com>
2025-07-25 12:43:04 +0900

    로컬 경로 설정 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,7 @@ import {
     BaseApiResponse
   } from './types';
   
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
   
   class ApiError extends Error {
     constructor(
